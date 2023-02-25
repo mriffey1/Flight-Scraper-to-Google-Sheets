32203cd4a43317efd67f52bbb16892e982fef47e
MeganRiffey <mriffe4@wgu.edu>
2023-02-25 13:51:37 -0500

    Cleanning up code and imports

--- flight_prices.py
+++ flight_prices.py
@@ -1,10 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
